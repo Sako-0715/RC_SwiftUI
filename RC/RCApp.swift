27d7a187bdbb92b841606bc2f7f2132204cbb9bd
Sako-0715 <sakou.0715@gmail.com>
2024-03-07 23:29:29 +0900

    Initial Commit

---
+++ RC/RCApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCApp.swift
+//  RC
+//
+//  Created by 酒匂竜也 on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct RCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
